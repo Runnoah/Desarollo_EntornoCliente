4474c4e957d56398863eb61a61f1b42627808584
Runoah <rubengarciacubero@gmail.com>
2026-01-15 15:32:25 +0100

    Update react

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,26 @@
 function App() {
 
-    const lowercaseClass = "text-lowercase"
-
-    const text = <h1>Bienvenido al multiverso JSX de Rubén</h1>
-
-    return <h1 className={lowercaseClass}>{text}</h1>
-
+    const users = [
+        { id: 1, name: 'Jose', role: 'Web Developer' },
+        { id: 2, name: 'Estefanía', role: 'Web Designer' },
+        { id: 3, name: 'Rubén', role: 'Team Leader' },
+    ]
+    
+    
+    return (
+        <>
+        <p>Lista de usuarios activos:</p>
+        <ul>
+        {users.map(function(user) {
+            return (
+                <li key={user.id}>
+                    {user.name} - {user.role}
+                </li>
+                )
+        })}
+        </ul>
+        </>
+    )
 }
-
 
 export default App
